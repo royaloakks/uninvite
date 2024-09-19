e621e0e021ab6f5a5bb46dbdcf0a819acc94206f
royaloakks <adamroyal@gmail.com>
2024-09-19 22:34:32 +0100

    Add functionality to stop all sounds with ESC key

--- src/SoundBoard.js
+++ src/SoundBoard.js
@@ -1,21 +1,33 @@
-import React, { useEffect } from 'react';
-import { Howl } from 'howler';
+import React, { useEffect, useState } from 'react';
+import { Howl, Howler } from 'howler';
 
 const SoundBoard = () => {
+  const [activeSounds, setActiveSounds] = useState([]);
+
   useEffect(() => {
     const sounds = {
-      'q': new Howl({ src: ['/sounds/SG-Guitar-1.mp3'] }),
-      'w': new Howl({ src: ['/sounds/SG-Guitar-2.mp3'] }),
-      'e': new Howl({ src: ['/sounds/sound3.mp3'] }),
-      'r': new Howl({ src: ['/sounds/sound4.mp3'] }),
+      'q': new Howl({ src: [`${process.env.PUBLIC_URL}/sounds/SG-Guitar-1.mp3`] }),
+      'w': new Howl({ src: [`${process.env.PUBLIC_URL}/sounds/SG-Guitar-2.mp3`] }),
+      'e': new Howl({ src: [`${process.env.PUBLIC_URL}/sounds/sound3.mp3`] }),
+      'r': new Howl({ src: [`${process.env.PUBLIC_URL}/sounds/sound4.mp3`] }),
       // Add more sounds for different keys as needed
     };
 
     const handleKeyPress = (event) => {
       const key = event.key.toLowerCase();
       if (sounds[key]) {
-        sounds[key].play();
+        const sound = sounds[key].play();
+        setActiveSounds(prev => [...prev, sound]);
+      } else if (key === 'escape') {
+        stopAllSounds();
       }
+    };
+
+    const stopAllSounds = () => {
+      activeSounds.forEach(soundId => {
+        Howler.stop(soundId);
+      });
+      setActiveSounds([]);
     };
 
     window.addEventListener('keydown', handleKeyPress);
@@ -23,12 +35,14 @@ const SoundBoard = () => {
     // Cleanup function
     return () => {
       window.removeEventListener('keydown', handleKeyPress);
+      stopAllSounds();
     };
-  }, []);
+  }, [activeSounds]);
 
   return (
     <div>
       <h1>Press Q, W, E, or R to play sounds!</h1>
+      <p>Press ESC to stop all sounds</p>
     </div>
   );
 };
