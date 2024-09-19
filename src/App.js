d0331cf316feb3cdcdfbbc8bfbd201002382c1bd
royaloakks <adamroyal@gmail.com>
2024-09-19 22:15:51 +0100

    Fix App.css import issue

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SoundBoard from './SoundBoard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SoundBoard />
+    </div>
+  );
+}
+
+export default App;
