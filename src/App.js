4ff68d2c88b31387b40e325a32913b802e467307
royaloakks <adamroyal@gmail.com>
2024-09-22 12:59:13 +0100

    Adjust styling for full-page background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import SoundBoard from './SoundBoard';
 import './App.css';
+import backgroundImage from './images/sgbg1-lo.jpg'; // Add this line
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: `url(${backgroundImage})`}}>
       <div className="soundboard-container">
         <SoundBoard />
       </div>
